7a28377ee7ecb12118a02f364d76d65768182c57
DrizzleDD <3279832141@qq.com>
2022-03-07 15:42:10 +0800

    在api的entity里，建dto包，用于接收前端传来的参数，建类UserMemberRegisterParamDTO

---
+++ msb-dongbao-api/msb-dongbao-ums-api/src/main/java/com/msb/dongbao/ums/entity/dto/UserMemberRegisterParamDTO.java
@@ -0,0 +1,30 @@
+package com.msb.dongbao.ums.entity.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+/**
+ * @Auther: WZY
+ * @Date: 2022/3/7 - 03 - 07 - 15:38
+ * @Description: com.msb.dongbao.ums.entity.dto
+ * @version: 1.0
+ */
+@Data
+@ToString
+public class UserMemberRegisterParamDTO {
+
+    @Size(min = 1,max = 8,message = "用户名长度在1-8之间")
+    private String username;
+
+    private String password;
+
+    private String icon;
+
+    @Email
+    private String email;
+
+    private String nickName;
+}
