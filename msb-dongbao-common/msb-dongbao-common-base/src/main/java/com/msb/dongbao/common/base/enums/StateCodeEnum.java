d0dfa95d8f724fb97a7940c127d5da1dcf4d18f2
DrizzleDD <3279832141@qq.com>
2022-03-08 14:34:46 +0800

    controller统一返回值:在common里面建enum枚举类，用来储存code状态码类型。

---
+++ msb-dongbao-common/msb-dongbao-common-base/src/main/java/com/msb/dongbao/common/base/enums/StateCodeEnum.java
@@ -0,0 +1,10 @@
+package com.msb.dongbao.common.base.enums;
+
+/**
+ * @Auther: WZY
+ * @Date: 2022/3/8 - 03 - 08 - 14:33
+ * @Description: com.msb.dongbao.common.base.enums
+ * @version: 1.0
+ */
+public class StateCodeEnum {
+}
